53d68c6a7c68c3948900a05aafd2528e3caf0dfa
Abhin Shah <Abhin02@users.noreply.github.com>
2015-09-18 02:33:48 +0530

    Update game_over.py

--- game_over.py
+++ game_over.py
@@ -9,7 +9,6 @@ display_height=x
 display_width=y
 DISPLAYSURF = pygame.display.set_mode((x, y))
 overImg= pygame.image.load('ben10background.png')
-x,y=makeText("Game Over", red,20,30)
 DISPLAYSURF.blit(overImg, (0, 0))
 pygame.display.set_caption('Game Over!')
 while True: # main game loop
