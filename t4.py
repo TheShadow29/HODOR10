e633019b476fbf9439a972b5fa5b8732b62a5195
Karan Chadha <karanchadhaiitb@gmail.com>
2015-09-17 14:31:33 +0530

    ben moving both sides and sprites class updated

--- t4.py
+++ t4.py
@@ -19,35 +19,39 @@ direction = 'right'
 
 execfile('sprites.py')
 WHITE = (255, 255, 255)
-a=0
-b=0
-ben = [sprites(a,b,'Images/sprites/ben_walk_right_FILES/0.png'), 
-       sprites(a,b,'Images/sprites/ben_walk_right_FILES/1.png'),
-       sprites(a,b,'Images/sprites/ben_walk_right_FILES/2.png'),
-       sprites(a,b,'Images/sprites/ben_walk_right_FILES/3.png'),
-       sprites(a,b,'Images/sprites/ben_walk_right_FILES/4.png'),
-       sprites(a,b,'Images/sprites/ben_walk_right_FILES/5.png'),
-       sprites(a,b,'Images/sprites/ben_walk_right_FILES/6.png'),
-       sprites(a,b,'Images/sprites/ben_walk_right_FILES/7.png')]
+
+ben = [sprites('Images/sprites/ben_walk_right_FILES/0.png'), 
+       sprites('Images/sprites/ben_walk_right_FILES/1.png'),
+       sprites('Images/sprites/ben_walk_right_FILES/2.png'),
+       sprites('Images/sprites/ben_walk_right_FILES/3.png'),
+       sprites('Images/sprites/ben_walk_right_FILES/4.png'),
+       sprites('Images/sprites/ben_walk_right_FILES/5.png'),
+       sprites('Images/sprites/ben_walk_right_FILES/6.png'),
+       sprites('Images/sprites/ben_walk_right_FILES/7.png')]
 
 pygame_img = None
 bgx = 10
 bgy = 10
-index = 0
+index1 = 0
+index2 = 0
 while True:
     DISPLAYSURF.fill(WHITE)
     pygame.event.pump()
     keys = pygame.key.get_pressed()
 
     DISPLAYSURF.blit(bgImg,(bgx,bgy))
-    pygame_img = ben[index%4].image
+    pygame_img = ben[index1%4].image
 
     if keys[pygame.K_RIGHT]:
         bgx-=8
-        index+=1
-        pygame_img = ben[index%4].image
+        index1+=1
+        pygame_img = ben[index1%4].image
+#        print 'right pressed'
     elif keys[pygame.K_LEFT]:
         bgx+=8
+        index2+=1
+        pygame_img = ben[4+index2%4].image
+#        print 'left pressed'
     DISPLAYSURF.blit(pygame_img,(330,500))
     for event in pygame.event.get():
         if event.type == QUIT:
